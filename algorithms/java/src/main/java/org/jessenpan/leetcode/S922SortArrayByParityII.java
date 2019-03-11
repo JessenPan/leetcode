b53ee7c000116ed1e434b41cba50084ab90eedfa
jessenpan <cdpanxinyu@jd.com>
2019-03-11 10:54:51 +0800

    submit the solution to the problem 922

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S922SortArrayByParityII.java
@@ -0,0 +1,25 @@
+package org.jessenpan.leetcode;
+
+/**
+ * @author jessenpan
+ * tag:array
+ */
+public class S922SortArrayByParityII {
+
+    public int[] sortArrayByParityII(int[] a) {
+        int[] result = new int[a.length];
+        int parityIndex = 0, oddIndex = 1;
+        int lengthOfA = a.length;
+        for (int i = 0; i < lengthOfA; i++) {
+            if (a[i] % 2 == 0) {
+                result[parityIndex] = a[i];
+                parityIndex = 2 + parityIndex;
+            } else {
+                result[oddIndex] = a[i];
+                oddIndex = 2 + oddIndex;
+            }
+        }
+        return result;
+    }
+}
+ 
---
+++ algorithms/java/src/test/java/org/jessenpan/leetcode/S922SortArrayByParityIITest.java
@@ -0,0 +1,24 @@
+package org.jessenpan.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+/**
+ * @author jessenpan
+ */
+public class S922SortArrayByParityIITest {
+
+    private S922SortArrayByParityII s922SortArrayByParityII = new S922SortArrayByParityII();
+
+    @Test
+    public void test1() {
+        int[] result = s922SortArrayByParityII.sortArrayByParityII(new int[] { 4, 2, 5, 7 });
+        assertArrayEquals(new int[] { 4, 5, 2, 7 }, result);
+    }
+
+    @Test
+    public void test2() {
+        int[] result = s922SortArrayByParityII.sortArrayByParityII(new int[] { 4, 1, 1, 0, 1, 0 });
+    }
+}
