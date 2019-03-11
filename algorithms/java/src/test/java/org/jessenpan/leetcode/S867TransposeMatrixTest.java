ee7a6c62f2bca03e32faea31408945eb58fa7e2c
jessenpan <cdpanxinyu@jd.com>
2019-03-11 14:46:22 +0800

    move the package path of problem 561

--- algorithms/java/src/test/java/org/jessenpan/leetcode/S867TransposeMatrixTest.java
+++ algorithms/java/src/test/java/org/jessenpan/leetcode/S867TransposeMatrixTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
  */
 public class S867TransposeMatrixTest {
 
-    private S867TransposeMatrix transposeMatrix = new S867TransposeMatrix();
+    private S844BackspaceStringCompare.S867TransposeMatrix transposeMatrix = new S844BackspaceStringCompare.S867TransposeMatrix();
 
     @Test
     public void test1() {
