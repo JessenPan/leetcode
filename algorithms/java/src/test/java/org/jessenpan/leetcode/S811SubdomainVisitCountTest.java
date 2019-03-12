1899b09e7a3303864be8b362478da203dd01b2de
jessenpan <cdpanxinyu@jd.com>
2019-03-12 22:23:10 +0800

    394

---
+++ algorithms/java/src/test/java/org/jessenpan/leetcode/S811SubdomainVisitCountTest.java
@@ -0,0 +1,29 @@
+package org.jessenpan.leetcode;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jessenpan
+ */
+public class S811SubdomainVisitCountTest {
+
+    private S811SubdomainVisitCount subdomainVisitCount = new S811SubdomainVisitCount();
+    
+    @Test
+    public void test1() {
+        List<String> subDomains = subdomainVisitCount.subdomainVisits(new String[] {"9001 discuss.leetcode.com"});
+        assertEquals(Arrays.asList("9001 discuss.leetcode.com", "9001 leetcode.com", "9001 com"),subDomains);
+    }
+
+    @Test
+    public void test2() {
+        List<String> subDomains = subdomainVisitCount.subdomainVisits(new String[] {"900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"});
+        assertEquals(Arrays.asList("901 mail.com","50 yahoo.com","900 google.mail.com","5 wiki.org","5 org","1 intel.mail.com","951 com"),subDomains);
+    }
+    
+}
