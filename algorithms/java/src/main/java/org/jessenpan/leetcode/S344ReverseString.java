529ca2f2cb309f205b6b836a12c427211d64f075
jessenpan <jessenpan@qq.com>
2019-03-13 22:37:50 +0800

    submit the solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S344ReverseString.java
@@ -0,0 +1,21 @@
+package org.jessenpan.leetcode;
+
+/**
+ * @author jessenpan
+ */
+public class S344ReverseString {
+
+    public void reverseString(char[] s) {
+        int loopTime = s.length / 2;
+        int leftIndex=0,rightIndex=s.length-1;
+        char tmp;
+        for (int i = 0; i < loopTime; i++) {
+            tmp=s[leftIndex];
+            s[leftIndex]=s[rightIndex];
+            s[rightIndex]=tmp;
+            leftIndex++;
+            rightIndex--;
+        }
+    }
+
+}
