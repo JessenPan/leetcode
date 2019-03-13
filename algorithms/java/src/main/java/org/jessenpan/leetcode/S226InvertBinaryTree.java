6fcce0c036048a8b589fda558bd1a65bbfbdeb86
jessenpan <jessenpan@qq.com>
2019-03-13 23:46:44 +0800

    submit the solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S226InvertBinaryTree.java
@@ -0,0 +1,43 @@
+package org.jessenpan.leetcode;
+
+/**
+ * @author jessenpan
+ * tag:tree,stack
+ */
+public class S226InvertBinaryTree {
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) { val = x; }
+
+        public TreeNode getLeft() {
+            return left;
+        }
+
+        public void setLeft(TreeNode left) {
+            this.left = left;
+        }
+
+        public TreeNode getRight() {
+            return right;
+        }
+
+        public void setRight(TreeNode right) {
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode newTreeNode = new TreeNode(root.val);
+        newTreeNode.left = invertTree(root.right);
+        newTreeNode.right = invertTree(root.left);
+        return newTreeNode;
+    }
+}
