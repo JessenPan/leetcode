93dead06ba25d006c2d81b4911e3b73e52494b86
jessenpan <jessenpan@qq.com>
2019-03-14 22:41:11 +0800

    submit the solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S107BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,59 @@
+package org.jessenpan.leetcode;
+
+import java.util.*;
+
+/**
+ * @author jessenpan
+ * tag:tree
+ */
+public class S107BinaryTreeLevelOrderTraversalII {
+
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) { val = x; }
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+        Stack<List<Integer>> multiLeveledStack = new Stack<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        multiLeveledStack.add(Collections.singletonList(root.val));
+        while (!queue.isEmpty()) {
+            List<Integer> theLevelList = new LinkedList<>();
+            List<TreeNode> theNextNode = new LinkedList<>();
+            while (!queue.isEmpty()) {
+                TreeNode theFather = queue.poll();
+                if (theFather.left != null) {
+                    theNextNode.add(theFather.left);
+                    theLevelList.add(theFather.left.val);
+                }
+                if (theFather.right != null) {
+                    theNextNode.add(theFather.right);
+                    theLevelList.add(theFather.right.val);
+                }
+            }
+            if (theLevelList.size() > 0) {
+                multiLeveledStack.push(theLevelList);
+            }
+            if (theNextNode.size() > 0) {
+                theNextNode.forEach(queue::offer);
+            }
+        }
+        return convertStack2List(multiLeveledStack);
+    }
+
+    private List<List<Integer>> convertStack2List(Stack<List<Integer>> multiLeveledStack) {
+        List<List<Integer>> lists = new LinkedList<>();
+        while(!multiLeveledStack.isEmpty()){
+            lists.add(multiLeveledStack.pop());
+        }
+        return lists;
+    }
+}
---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S429NaryTreeLevelOrderTraversal.java
@@ -0,0 +1,53 @@
+package org.jessenpan.leetcode;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * @author jessenpan
+ * tag:tree
+ */
+public class S429NaryTreeLevelOrderTraversal {
+
+    public static class Node {
+
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int val, List<Node> children) {
+            this.val = val;
+            this.children = children;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(Node root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+        List<List<Integer>> multiLeveledList = new LinkedList<>();
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        multiLeveledList.add(Collections.singletonList(root.val));
+        while (!queue.isEmpty()) {
+            List<Integer> theLevelList = new LinkedList<>();
+            List<Node> theNextNode = new LinkedList<>();
+            while (!queue.isEmpty()) {
+                List<Node> theChildren = queue.poll().children;
+                if (theChildren != null && theChildren.size() > 0) {
+                    theNextNode.addAll(theChildren);
+                    theLevelList.addAll(theChildren.parallelStream().map(node -> node.val).collect(Collectors.toList()));
+                }
+            }
+            if (theLevelList.size() > 0) {
+                multiLeveledList.add(theLevelList);
+            }
+            if (theNextNode.size() > 0) {
+                theNextNode.forEach(queue::offer);
+            }
+        }
+        return multiLeveledList;
+    }
+
+}
