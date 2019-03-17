78ac0954da3edffe4650dbf66c4a22ed6a2edc68
jessenpan <jessenpan@qq.com>
2019-03-17 15:41:59 +0800

    submit the solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S538ConvertBSTToGreaterTree.java
@@ -0,0 +1,36 @@
+package org.jessenpan.leetcode;
+
+/**
+ * @author jessenpan
+ * tag:tree
+ */
+public class S538ConvertBSTToGreaterTree {
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode convertBST(TreeNode root) {
+        return traverseTree(root);
+    }
+
+    private int sumOfGreaterNode = 0;
+
+    private TreeNode traverseTree(TreeNode node) {
+        if (node == null) {
+            return null;
+        }
+        TreeNode newRightNode = traverseTree(node.right);
+        sumOfGreaterNode += node.val;
+        TreeNode newNode = new TreeNode(sumOfGreaterNode);
+        newNode.right = newRightNode;
+        newNode.left = traverseTree(node.left);
+        return newNode;
+    }
+
+}
