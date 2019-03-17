49fcb41c0b155ca324ab0274a090c681bed963e0
jessenpan <jessenpan@qq.com>
2019-03-17 13:58:35 +0800

    submit the solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S100SameTree.java
@@ -0,0 +1,34 @@
+package org.jessenpan.leetcode;
+
+/**
+ * @author jessenpan
+ * tag:tree
+ */
+public class S100SameTree {
+
+    public class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        boolean isLeftTreeSame = isSameTree(p.left, q.left);
+        if (!isLeftTreeSame) {
+            return false;
+        }
+        return isSameTree(p.right, q.right);
+    }
+}
