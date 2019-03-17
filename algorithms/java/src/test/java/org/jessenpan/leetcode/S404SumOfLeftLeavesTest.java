bed5b0f20552f61ef5141e139aed5d32a1197283
jessenpan <jessenpan@qq.com>
2019-03-17 10:57:45 +0800

    submit the solution

---
+++ algorithms/java/src/test/java/org/jessenpan/leetcode/S404SumOfLeftLeavesTest.java
@@ -0,0 +1,23 @@
+package org.jessenpan.leetcode;
+
+import org.jessenpan.leetcode.S404SumOfLeftLeaves.TreeNode;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jessenpan
+ */
+public class S404SumOfLeftLeavesTest {
+
+    private S404SumOfLeftLeaves sumOfLeftLeaves = new S404SumOfLeftLeaves();
+
+    @Test
+    public void test1() {
+        TreeNode node1 = new TreeNode(20, new TreeNode(15), new TreeNode(7));
+        TreeNode root = new TreeNode(3, new TreeNode(9), node1);
+        int theSum = sumOfLeftLeaves.sumOfLeftLeaves(root);
+        assertEquals(24, theSum);
+    }
+
+}
