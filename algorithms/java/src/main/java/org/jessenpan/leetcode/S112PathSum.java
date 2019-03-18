92d0de77f943de59d57881a3cad0f04cc9aa4746
jessenpan <cdpanxinyu@jd.com>
2019-03-18 14:43:05 +0800

    submit solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S112PathSum.java
@@ -0,0 +1,45 @@
+package org.jessenpan.leetcode;
+
+/**
+ * @author jessenpan
+ * tag:tree
+ */
+public class S112PathSum {
+
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        this.targetSum = sum;
+        return countToSum(root, 0);
+    }
+
+    private int targetSum;
+
+    public boolean countToSum(TreeNode node, int currentSum) {
+        if (node == null) {
+            return false;
+        }
+        currentSum=node.val+currentSum;
+        if (node.left == null && node.right == null) {
+            //leaf
+            if (currentSum == targetSum) {
+                return true;
+            }
+        }
+        if(countToSum(node.left,currentSum)){
+            return true;
+        }
+        return countToSum(node.right, currentSum);
+    }
+
+}
