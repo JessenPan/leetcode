cf6d74dba51853c7f4824b9f1eb2f9ac13d574da
jessenpan <cdpanxinyu@jd.com>
2019-03-18 15:32:26 +0800

    submit solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S572SubtreeOfAnotherTree.java
@@ -0,0 +1,57 @@
+package org.jessenpan.leetcode;
+
+/**
+ * @author jessenpan
+ * tag:tree
+ */
+public class S572SubtreeOfAnotherTree {
+
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) { val = x; }
+    }
+
+    private TreeNode targetSubTree;
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        this.targetSubTree = t;
+        return isSubtree(s);
+    }
+
+    private boolean isSubtree(TreeNode node) {
+        if (node == null) {
+            return false;
+        }
+        if (node.val == targetSubTree.val) {
+            if (isEqualOfTree(node, targetSubTree)) {
+                return true;
+            }
+        }
+        if (isSubtree(node.left)) {
+            return true;
+        }
+        return isSubtree(node.right);
+    }
+
+    private boolean isEqualOfTree(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+        if (node1.val != node2.val) {
+            return false;
+        }
+        if (!isEqualOfTree(node1.left, node2.left)) {
+            return false;
+        }
+
+        return isEqualOfTree(node1.right, node2.right);
+    }
+
+}
