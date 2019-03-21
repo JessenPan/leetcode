524176fa02ae0aa6bf19fdd5e2845a44748bcf6b
jessenpan <jessenpan@qq.com>
2019-03-21 23:18:33 +0800

    submit the solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S258AddDigits.java
@@ -0,0 +1,20 @@
+package org.jessenpan.leetcode;
+
+/**
+ * @author jessenpan
+ * tag:math
+ * //TODO lean how to inference
+ */
+public class S258AddDigits {
+        
+    public int addDigits(int num){
+        if(num==0){
+            return 0;
+        }
+        if(num%9==0){
+            return 9;
+        }
+        
+        return num%9;
+    }
+}
