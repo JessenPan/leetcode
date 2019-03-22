76af0d1fb9bcbab73f28adf884678b7e14c20bb4
jessenpan <cdpanxinyu@jd.com>
2019-03-22 13:04:42 +0800

    submit solution

---
+++ algorithms/java/src/test/java/org/jessenpan/leetcode/S231PowerOfTwoTest.java
@@ -0,0 +1,33 @@
+package org.jessenpan.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author jessenpan
+ */
+public class S231PowerOfTwoTest {
+
+    private S231PowerOfTwo powerOfTwo = new S231PowerOfTwo();
+
+    @Test
+    public void test1() {
+        boolean isPowerOfTwo = powerOfTwo.isPowerOfTwo(4);
+        assertTrue(isPowerOfTwo);
+    }
+
+    @Test
+    public void test2() {
+        boolean isPowerOfTwo=powerOfTwo.isPowerOfTwo(2147483646);
+        assertFalse(isPowerOfTwo);
+    }
+
+    @Test
+    public void test3() {
+        boolean isPowerOfTwo=powerOfTwo.isPowerOfTwo(6);
+        assertFalse(isPowerOfTwo);
+    }
+
+}
