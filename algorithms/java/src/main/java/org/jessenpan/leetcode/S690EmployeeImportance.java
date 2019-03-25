850c99b526714d962fc8d1dcb91deb8311e4aa3d
jessenpan <jessenpan@qq.com>
2019-03-25 23:37:11 +0800

    submit the solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S690EmployeeImportance.java
@@ -0,0 +1,41 @@
+package org.jessenpan.leetcode;
+
+import java.util.*;
+
+/**
+ * @author jessenpan
+ * tag:depth first search
+ */
+public class S690EmployeeImportance {
+
+    public static class Employee {
+
+        // It's the unique id of each node;
+        // unique id of this employee
+        public int id;
+        // the importance value of this employee
+        public int importance;
+        // the id of direct subordinates
+        public List<Integer> subordinates;
+    }
+
+    public int getImportance(List<Employee> employees, int id) {
+        Map<Integer, Employee> employeeMap = new HashMap<>();
+        for (Employee employee : employees) {
+            employeeMap.put(employee.id, employee);
+        }
+        Queue<Employee> queue = new LinkedList<>();
+        queue.offer(employeeMap.get(id));
+        int importanceOfId = 0;
+        while (!queue.isEmpty()) {
+            Employee employee = queue.poll();
+            importanceOfId += employee.importance;
+            if (employee.subordinates != null) {
+                for (Integer subordinate : employee.subordinates) {
+                    queue.offer(employeeMap.get(subordinate));
+                }
+            }
+        }
+        return importanceOfId;
+    }
+}
