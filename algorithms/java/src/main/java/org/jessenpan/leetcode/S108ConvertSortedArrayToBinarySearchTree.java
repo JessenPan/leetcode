318056779b5c11db6b9dfe34b259018257a17872
jessenpan <jessenpan@qq.com>
2019-03-25 23:22:47 +0800

    submit the solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S108ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,38 @@
+package org.jessenpan.leetcode;
+
+/**
+ * @author jessenpan
+ * tag:deep-first search
+ */
+public class S108ConvertSortedArrayToBinarySearchTree {
+
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return new TreeNode(nums[0]);
+        }
+        int parentNodePosition = length / 2;
+        TreeNode node = new TreeNode(nums[parentNodePosition]);
+        int[] leftArray = new int[parentNodePosition];
+        System.arraycopy(nums, 0, leftArray, 0, parentNodePosition);
+        node.left = sortedArrayToBST(leftArray);
+        if (length - parentNodePosition - 1 > 0) {
+            int[] rightArray = new int[length - parentNodePosition - 1];
+            System.arraycopy(nums, parentNodePosition + 1, rightArray, 0, length - parentNodePosition - 1);
+            node.right = sortedArrayToBST(rightArray);
+        }
+        return node;
+    }
+}
---
+++ algorithms/java/src/test/java/org/jessenpan/leetcode/S108ConvertSortedArrayToBinarySearchTreeTest.java
@@ -0,0 +1,17 @@
+package org.jessenpan.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @author jessenpan
+ */
+public class S108ConvertSortedArrayToBinarySearchTreeTest {
+
+    private S108ConvertSortedArrayToBinarySearchTree convertSortedArrayToBinarySearchTree = new S108ConvertSortedArrayToBinarySearchTree();
+    
+    @Test
+    public void test1(){
+        convertSortedArrayToBinarySearchTree.sortedArrayToBST(new int[] { -10, -3, 0, 5, 9 });
+    }
+    
+}
