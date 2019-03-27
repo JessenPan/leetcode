c6577b83a2f5f73abd6fd95099d4b3759abfbbe8
jessenpan <jessenpan@qq.com>
2019-03-27 22:22:41 +0800

    submit the solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S461HammingDistance.java
@@ -0,0 +1,18 @@
+package org.jessenpan.leetcode;
+
+/**
+ * @author jessenpan
+ * tag:bit
+ */
+public class S461HammingDistance {
+
+    public int hammingDistance(int x, int y) {
+        int m = x ^ y;
+        int distance = 0;
+        while (m != 0) {
+            distance += m % 2;
+            m = m >> 1;
+        }
+        return distance;
+    }
+}
---
+++ algorithms/java/src/test/java/org/jessenpan/leetcode/S461HammingDistanceTest.java
@@ -0,0 +1,20 @@
+package org.jessenpan.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jessenpan
+ */
+public class S461HammingDistanceTest {
+
+    private S461HammingDistance hammingDistance = new S461HammingDistance();
+
+    @Test
+    public void test() {
+        int distance = hammingDistance.hammingDistance(1, 4);
+        assertEquals(2, distance);
+    }
+
+}
