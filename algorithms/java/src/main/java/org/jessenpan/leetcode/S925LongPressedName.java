2da63dad3b43eaab8cca2b4a90706a8d6632a6bd
jessenpan <cdpanxinyu@jd.com>
2019-03-28 21:02:00 +0800

    submit solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S925LongPressedName.java
@@ -0,0 +1,13 @@
+package org.jessenpan.leetcode;
+
+/**
+ * @author jessenpan
+ * tag:string
+ */
+public class S925LongPressedName {
+
+    public boolean isLongPressedName(String name, String typed) {
+        //TODO not implement
+        return true;
+    }
+}
