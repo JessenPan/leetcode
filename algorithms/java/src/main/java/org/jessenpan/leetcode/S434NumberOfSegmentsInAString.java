38cd66639e978db1a11b4669c711dda740be28dd
jessenpan <jessenpan@qq.com>
2019-03-29 00:54:51 +0800

    submit the solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S434NumberOfSegmentsInAString.java
@@ -0,0 +1,21 @@
+package org.jessenpan.leetcode;
+
+/**
+ * @author jessenpan
+ * tag:string
+ */
+public class S434NumberOfSegmentsInAString {
+
+    public int countSegments(String s) {
+        if (s.isEmpty() || s.trim().isEmpty()) {
+            return 0;
+        }
+        String[] words = s.trim().split(",");
+        int sum = 0;
+        for (String word : words) {
+            sum += word.trim().split(" ").length;
+        }
+        return sum;
+    }
+
+}
