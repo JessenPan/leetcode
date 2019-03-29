aa0dc1b60f58770303fddbf1bf3a1b93374ab4e9
jessenpan <jessenpan@qq.com>
2019-03-30 00:27:21 +0800

    submit the solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S191NumberOf1Bits.java
@@ -0,0 +1,17 @@
+package org.jessenpan.leetcode;
+
+/**
+ * @author jessenpan
+ * tag:bit
+ */
+public class S191NumberOf1Bits {
+
+    public int hammingWeight(int n) {
+        int count = 0;
+        for(int i = 0; i < 32; i++){
+            count += 1&(n>>i);
+        }
+        return count;
+    }
+
+}
---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S762PrimeNumberOfSetBitsInBinaryRepresentation.java
@@ -0,0 +1,13 @@
+package org.jessenpan.leetcode;
+
+/**
+ * @author jessenpan
+ * tag:bit
+ */
+public class S762PrimeNumberOfSetBitsInBinaryRepresentation {
+
+    public int countPrimeSetBits(int L, int R) {
+        //TODO need to implement it
+        return 1;
+    }
+}
