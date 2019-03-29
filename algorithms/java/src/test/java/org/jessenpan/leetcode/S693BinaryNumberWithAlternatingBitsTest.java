523ce38a244439b4c21602bbbe82c4d2ea21dcb1
jessenpan <jessenpan@qq.com>
2019-03-29 23:28:30 +0800

    submit the solution

---
+++ algorithms/java/src/test/java/org/jessenpan/leetcode/S693BinaryNumberWithAlternatingBitsTest.java
@@ -0,0 +1,38 @@
+package org.jessenpan.leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author jessenpan
+ */
+public class S693BinaryNumberWithAlternatingBitsTest {
+
+    private S693BinaryNumberWithAlternatingBits binaryNumberWithAlternatingBits = new S693BinaryNumberWithAlternatingBits();
+
+    @Test
+    public void test1() {
+        boolean hasAlter = binaryNumberWithAlternatingBits.hasAlternatingBits(5);
+        assertTrue(hasAlter);
+    }
+
+    @Test
+    public void test2() {
+        boolean hasAlter = binaryNumberWithAlternatingBits.hasAlternatingBits(7);
+        assertFalse(hasAlter);
+    }
+
+    @Test
+    public void test3() {
+        boolean hasAlter = binaryNumberWithAlternatingBits.hasAlternatingBits(11);
+        assertFalse(hasAlter);
+    }
+
+    @Test
+    public void test4() {
+        boolean hasAlter = binaryNumberWithAlternatingBits.hasAlternatingBits(10);
+        assertTrue(hasAlter);
+    }
+}
