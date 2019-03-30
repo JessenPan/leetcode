82fb78dd0f2bddf003ba32ead70dec1f6371758f
jessenpan <jessenpan@qq.com>
2019-03-30 11:46:35 +0800

    submit the solution

--- algorithms/java/src/main/java/org/jessenpan/leetcode/S191NumberOf1Bits.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S191NumberOf1Bits.java
@@ -6,7 +6,6 @@ package org.jessenpan.leetcode;
  */
 public class S191NumberOf1Bits {
 
-    //TODO implement it
     public int hammingWeight(int n) {
         int count = 0;
         for(int i = 0; i < 32; i++){
---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/S268MissingNumber.java
@@ -0,0 +1,16 @@
+package org.jessenpan.leetcode;
+
+/**
+ * @author jessenpan
+ * tag:string
+ */
+public class S268MissingNumber {
+
+    public int missingNumber(int[] nums) {
+        int sum = 0, length = nums.length;
+        for (int i = 0; i < length; i++) {
+            sum += nums[i];
+        }
+        return length * (length + 1) / 2 - sum;
+    }
+}
