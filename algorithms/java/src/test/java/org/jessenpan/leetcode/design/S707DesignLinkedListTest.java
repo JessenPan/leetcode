1006eb0d9ffe00057c05a8612ff35068ae2a3cea
jessenpan <jessenpan@qq.com>
2019-03-30 21:58:14 +0800

    submit the solution

---
+++ algorithms/java/src/test/java/org/jessenpan/leetcode/design/S707DesignLinkedListTest.java
@@ -0,0 +1,40 @@
+package org.jessenpan.leetcode.design;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jessenpan
+ */
+public class S707DesignLinkedListTest {
+
+    private S707DesignLinkedList.MyLinkedList linkedList = new S707DesignLinkedList.MyLinkedList();
+
+    @Test
+    public void test() {
+        linkedList.addAtHead(1);
+        linkedList.addAtTail(3);
+        linkedList.addAtIndex(1, 2);
+        assertEquals(2, linkedList.get(1));
+        linkedList.deleteAtIndex(1);
+        assertEquals(3, linkedList.get(1));
+    }
+
+    @Test
+    public void test1() {
+        linkedList.addAtHead(1);
+        linkedList.addAtIndex(1, 2);
+        assertEquals(2, linkedList.get(1));
+        assertEquals(1, linkedList.get(0));
+        assertEquals(-1, linkedList.get(2));
+    }
+
+    @Test
+    public void test2() {
+        linkedList.addAtHead(38);
+        linkedList.addAtHead(45);
+        linkedList.deleteAtIndex(2);
+    }
+
+}
