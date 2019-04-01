047be841c482a747005abb72b0ef2295232dd164
jessenpan <jessenpan@qq.com>
2019-04-01 22:52:09 +0800

    submit solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/math/S812LargestTriangleArea.java
@@ -0,0 +1,13 @@
+package org.jessenpan.leetcode.math;
+
+/**
+ * @author jessenpan
+ * tag:math
+ */
+public class S812LargestTriangleArea {
+    
+    public double largestTriangleArea(int[][] points) {
+
+    }
+
+}
