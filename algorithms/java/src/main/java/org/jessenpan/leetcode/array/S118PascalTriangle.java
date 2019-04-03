c7d03437d4dd9509a5c7fb3eb619b24c57576f22
jessenpan <jessenpan@qq.com>
2019-04-03 23:32:41 +0800

    submit solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/array/S118PascalTriangle.java
@@ -0,0 +1,43 @@
+package org.jessenpan.leetcode.array;
+
+import java.util.*;
+
+/**
+ * @author jessenpan
+ * tag:math
+ */
+public class S118PascalTriangle {
+
+    public List<List<Integer>> generate(int numRows) {
+        if (numRows == 0) {
+            return Collections.emptyList();
+        }
+
+        List<List<Integer>> pascalTriangle = new ArrayList<>();
+        for (int row = 1; row <= numRows; row++) {
+            if (row == 1) {
+                pascalTriangle.add(Collections.singletonList(1));
+                continue;
+            }
+            if (row == 2) {
+                pascalTriangle.add(Arrays.asList(1, 1));
+                continue;
+            }
+            List<Integer> rowList = new LinkedList<>();
+            for (int i = 0; i < row; i++) {
+                if (i == 0) {
+                    rowList.add(1);
+                    continue;
+                }
+                if (i == (row - 1)) {
+                    rowList.add(1);
+                    continue;
+                }
+                rowList.add(pascalTriangle.get(row - 2).get(i - 1) + pascalTriangle.get(row - 2).get(i));
+            }
+            pascalTriangle.add(rowList);
+        }
+        return pascalTriangle;
+    }
+
+}
---
+++ algorithms/java/src/test/java/org/jessenpan/leetcode/math/S118PascalTriangleTest.java
@@ -0,0 +1,20 @@
+package org.jessenpan.leetcode.math;
+
+import org.jessenpan.leetcode.array.S118PascalTriangle;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @author jessenpan
+ */
+public class S118PascalTriangleTest {
+
+    private S118PascalTriangle pascalTriangle = new S118PascalTriangle();
+
+    @Test
+    public void test1() {
+        List<List<Integer>> list = pascalTriangle.generate(5);
+    }
+
+}
