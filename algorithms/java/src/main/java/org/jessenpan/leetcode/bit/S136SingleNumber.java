323f73a328364d2eda9a5dbcf14d6b68cfe73b3d
jessenpan <cdpanxinyu@jd.com>
2019-04-05 22:00:51 +0800

    submit the solution

--- algorithms/java/src/main/java/org/jessenpan/leetcode/bit/S136SingleNumber.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/bit/S136SingleNumber.java
@@ -6,6 +6,16 @@ package org.jessenpan.leetcode.bit;
  */
 public class S136SingleNumber {
 
+    /**
+     * 异或的运用
+     * 
+     * 交换律:a^b^c=c^b^a
+     *  a^0=a
+     *  a^a=0
+     * 
+     * @param nums origin array
+     * @return the single number
+     */
     public int singleNumber(int[] nums) {
         int a = 0;
         for (int num : nums) {
