b542b5500cb333593dd94fdbf315347e19e60b08
jessenpan <cdpanxinyu@jd.com>
2019-04-10 21:37:57 +0800

    submit the solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/hashmap/S205IsomorphicStrings.java
@@ -0,0 +1,12 @@
+package org.jessenpan.leetcode.hashmap;
+
+/**
+ * @author jessenpan
+ * tag:hashmap
+ */
+public class S205IsomorphicStrings {
+
+    public boolean isIsomorphic(String s, String t) {
+
+    }
+}
