51d5650ee76dca5df0e95f50eda64ab489d89e05
jessenpan <cdpanxinyu@jd.com>
2019-04-13 15:50:35 +0800

    submit the solution

--- algorithms/java/src/main/java/org/jessenpan/leetcode/tree/S501FindModeInBinarySearchTree.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/tree/S501FindModeInBinarySearchTree.java
@@ -1,15 +1,14 @@
 package org.jessenpan.leetcode.tree;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author jessenpan
  * tag:tree
+ * 二叉搜索树的中序遍历是一个递增数组
  */
 public class S501FindModeInBinarySearchTree {
-
-    //TODO implement it
 
     public static class TreeNode {
 
@@ -23,7 +22,39 @@ public class S501FindModeInBinarySearchTree {
     }
 
     public int[] findMode(TreeNode root) {
-        Map<Integer, Integer> count = new HashMap<>();
-        return null;
+        countMode(root);
+        return nums.stream().mapToInt(t -> t).toArray();
+    }
+
+    private int max = 0;
+    private List<Integer> nums = new ArrayList<>();
+
+    private int pre = 0;
+    private int count = 0;
+
+    private void countMode(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        countMode(node.left);
+        if (count == 0) {
+            count++;
+            pre = node.val;
+        } else {
+            if (pre == node.val) {
+                count++;
+            } else {
+                pre = node.val;
+                count = 1;
+            }
+        }
+        if (count > max) {
+            nums.clear();
+            nums.add(pre);
+            max = count;
+        } else if (max == count) {
+            nums.add(pre);
+        }
+        countMode(node.right);
     }
 }
