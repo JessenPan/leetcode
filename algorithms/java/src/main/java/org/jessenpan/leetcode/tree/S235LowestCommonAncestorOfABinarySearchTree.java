139c99909b3c0b25f45269d2ac9c2f23ed4157c9
jessenpan <cdpanxinyu@jd.com>
2019-04-24 18:45:34 +0800

    submit the solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/tree/S235LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,30 @@
+package org.jessenpan.leetcode.tree;
+
+/**
+ * @author jessenpan
+ * tag:tree
+ */
+public class S235LowestCommonAncestorOfABinarySearchTree {
+
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            this.val = x;
+        }
+
+        TreeNode(int x, TreeNode left, TreeNode right) {
+            this.val = x;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+    }
+
+}
