a960c6d663b0f81d897c36ea6eb9eca4061de5fb
jessenpan <jessenpan@qq.com>
2019-04-30 20:05:04 +0800

    submit solution

---
+++ algorithms/java/src/test/java/org/jessenpan/leetcode/hashmap/S447NumberOfBoomerangsTest.java
@@ -0,0 +1,20 @@
+package org.jessenpan.leetcode.hashmap;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author jessenpan
+ */
+public class S447NumberOfBoomerangsTest {
+
+    S447NumberOfBoomerangs numberOfBoomerangs = new S447NumberOfBoomerangs();
+
+    @Test
+    public void test1() {
+        int num = numberOfBoomerangs.numberOfBoomerangs(new int[][] { { 0, 0 }, { 1, 0 }, { 2, 0 } });
+        assertEquals(2, num);
+    }
+
+}
