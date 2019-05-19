7d9d32222f58d134390fd167399c453ce6dd7f00
jessenpan <cdpanxinyu@jd.com>
2019-05-19 20:33:56 +0800

    submit the solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/backtracking/S691StickersToSpellWord.java
@@ -0,0 +1,12 @@
+package org.jessenpan.leetcode.backtracking;
+
+/**
+ * @author jessenpan
+ * tag:backtracing
+ */
+public class S691StickersToSpellWord {
+
+    public int minStickers(String[] stickers, String target) {
+        return 1;
+    }
+}
