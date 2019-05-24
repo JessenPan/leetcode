6294f3899f47ea03558d7ee118351d2a4804667c
jessenpan <jessenpan@qq.com>
2019-05-24 21:52:46 +0800

    submit solution

--- algorithms/java/src/main/java/org/jessenpan/leetcode/array/S287FindTheDuplicateNumber.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/array/S287FindTheDuplicateNumber.java
@@ -7,6 +7,18 @@ package org.jessenpan.leetcode.array;
 public class S287FindTheDuplicateNumber {
 
     public int findDuplicate(int[] nums) {
-        
+        int duplicate = -1;
+        int length = nums.length;
+        int index;
+        for (int i = 0; i < length; i++) {
+            index = Math.abs(nums[i]) - 1;
+            int val = nums[index];
+            if (val < 0) {
+                duplicate = Math.abs(nums[i]);
+            } else {
+                nums[index] = -nums[index];
+            }
+        }
+        return duplicate;
     }
 }
