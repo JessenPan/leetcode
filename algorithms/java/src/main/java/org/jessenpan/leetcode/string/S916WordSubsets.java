bc877f539ac82328820d22c568cb73d8501d35c9
jessenpan <jessenpan@qq.com>
2019-06-15 18:02:43 +0800

    submit solution

--- algorithms/java/src/main/java/org/jessenpan/leetcode/string/S916WordSubsets.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/string/S916WordSubsets.java
@@ -1,7 +1,9 @@
 package org.jessenpan.leetcode.string;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author jessenpan
@@ -13,24 +15,37 @@ public class S916WordSubsets {
         int[] cntMap = new int[26];
         for (String bWord : B) {
             int[] map = new int[26];
-            for (char ch : bWord.toCharArray())
+            for (char ch : bWord.toCharArray()) {
                 map[ch - 'a']++;
-            for (int i = 0; i < 26; ++i)
+            }
+            for (int i = 0; i < 26; ++i) {
                 cntMap[i] = Math.max(cntMap[i], map[i]);
+            }
         }
+
+        Map<String, int[]> record = new HashMap<>();
 
         List<String> result = new ArrayList<>();
         for (String aWord : A) {
-            int[] map = new int[26];
-            for (char ch : aWord.toCharArray())
-                map[ch - 'a']++;
+
+            int[] map = record.get(aWord);
+            if (map == null) {
+                map = new int[26];
+                record.put(aWord, map);
+                for (char ch : aWord.toCharArray()) {
+                    map[ch - 'a']++;
+                }
+            }
             boolean isFound = true;
-            for (int i = 0; i < 26; ++i)
-                if(cntMap[i] > map[i]) {
+            for (int i = 0; i < 26; ++i) {
+                if (cntMap[i] > map[i]) {
                     isFound = false;
                     break;
                 }
-            if (isFound) result.add(aWord);
+            }
+            if (isFound) {
+                result.add(aWord);
+            }
         }
         return result;
     }
