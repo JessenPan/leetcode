9b5e9b6a41cf487383bffb22a9254f4cd61fa152
jessenpan <cdpanxinyu@jd.com>
2019-06-30 17:12:18 +0800

    submit the solution

--- algorithms/java/src/main/java/org/jessenpan/leetcode/heap/S313SuperUglyNumber.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/heap/S313SuperUglyNumber.java
@@ -6,28 +6,30 @@ package org.jessenpan.leetcode.heap;
  */
 public class S313SuperUglyNumber {
 
+    //TODO study
+
     public int nthSuperUglyNumber(int n, int[] primes) {
-        int[] num = new int[n];
-        num[0] = 1;
+        int[] nums = new int[n];
+        nums[0] = 1;
         int primesLen = primes.length;
-        int[] idx = new int[primesLen];
+        int[] idxs = new int[primesLen];
 
         for (int i = 1; i < n; i++) {
-            int min = min(num, idx, primes);
+            int min = min(nums, idxs, primes);
             for (int j = 0; j < primesLen; j++) {
-                if (min == num[idx[j]] * primes[j])
-                    idx[j] = idx[j] + 1;
+                if (min == nums[idxs[j]] * primes[j])
+                    idxs[j] = idxs[j] + 1;
             }
-            num[i] = min;
+            nums[i] = min;
         }
-        return num[n - 1];
+        return nums[n - 1];
     }
 
-    private int min(int[] num, int[] idx, int[] primes) {
-        int res = num[idx[0]] * primes[0];
+    private int min(int[] nums, int[] idxs, int[] primes) {
+        int res = nums[idxs[0]] * primes[0];
         for (int i = 1; i < primes.length; i++) {
-            if (num[idx[i]] * primes[i] < res) {
-                res = num[idx[i]] * primes[i];
+            if (nums[idxs[i]] * primes[i] < res) {
+                res = nums[idxs[i]] * primes[i];
             }
         }
         return res;
