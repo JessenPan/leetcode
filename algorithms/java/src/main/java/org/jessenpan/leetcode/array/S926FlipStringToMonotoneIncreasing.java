17e4d272cd88ab12548339c5d127772622eae82b
jessenpan <jessenpan@qq.com>
2019-07-14 19:39:01 +0800

    submit solution

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/array/S926FlipStringToMonotoneIncreasing.java
@@ -0,0 +1,13 @@
+package org.jessenpan.leetcode.array;
+
+/**
+ * @author jessenpan
+ * tag:array
+ */
+public class S926FlipStringToMonotoneIncreasing {
+
+    public int minFlipsMonoIncr(String s) {
+        
+    }
+
+}
