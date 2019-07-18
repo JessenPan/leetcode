1a70444ca4b8549baaaaa2b8fb57cf80bb6bb545
jessenpan <cdpanxinyu@jd.com>
2019-07-18 17:17:37 +0800

    submit the solution

--- algorithms/java/src/main/java/org/jessenpan/leetcode/array/S33SearchInRotatedSortedArray.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/array/S33SearchInRotatedSortedArray.java
@@ -14,12 +14,14 @@ public class S33SearchInRotatedSortedArray {
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < nums[right]) {
+                //右边是有序的
                 if (nums[mid] < target && target <= nums[right]) {
                     left = mid + 1;
                 } else {
                     right = mid - 1;
                 }
             } else {
+                //左边是有序的
                 if (nums[left] <= target && target < nums[mid]) {
                     right = mid - 1;
                 } else {
