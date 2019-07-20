34db8db518a0d335994012da5232aed38fbbbeee
jessenpan <cdpanxinyu@jd.com>
2019-07-20 22:58:51 +0800

    submit the solution

--- algorithms/java/src/main/java/org/jessenpan/leetcode/array/S31NextPermutation.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/array/S31NextPermutation.java
@@ -9,7 +9,7 @@ public class S31NextPermutation {
     //TODO 
     public void nextPermutation(int[] nums) {
         int i = nums.length - 2;
-        while (i >= 0 && nums[i + 1] <= nums[i]) {
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
         if (i >= 0) {
