9e88152281a273b47d66f2ddca3016710bfef20b
jessenpan <cdpanxinyu@jd.com>
2019-07-21 17:35:03 +0800

    submit the solution

--- algorithms/java/src/main/java/org/jessenpan/leetcode/array/S713SubarrayProductLessThanK.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/array/S713SubarrayProductLessThanK.java
@@ -6,20 +6,21 @@ package org.jessenpan.leetcode.array;
  */
 public class S713SubarrayProductLessThanK {
 
+    //TODO 
     public int numSubarrayProductLessThanK(int[] nums, int k) {
 
         if (k <= 1) {
             return 0;
         }
-        int prod = 1, ans = 0, left = 0;
+        int sum = 1, num = 0, left = 0;
         for (int right = 0; right < nums.length; right++) {
-            prod *= nums[right];
-            while (prod >= k) {
-                prod /= nums[left++];
+            sum *= nums[right];
+            while (sum >= k) {
+                sum /= nums[left++];
             }
-            ans += right - left + 1;
+            num += right - left + 1;
         }
-        return ans;
+        return num;
     }
 
 }
