cfa276f551e35291f25be87602cd58a54fbb2dcc
jessenpan <jessenpan@qq.com>
2019-07-24 22:22:32 +0800

    submit solution

---
+++ algorithms/java/src/test/java/org/jessenpan/leetcode/hashmap/S781RabbitsInForestTest.java
@@ -0,0 +1,43 @@
+package org.jessenpan.leetcode.hashmap;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author jessenpan
+ */
+public class S781RabbitsInForestTest {
+
+    private S781RabbitsInForest rabbitsInForest = new S781RabbitsInForest();
+
+    @Test
+    public void test1() {
+        int num = rabbitsInForest.numRabbits(new int[] { 1, 1, 2 });
+        Assert.assertEquals(5, num);
+    }
+
+    @Test
+    public void test2() {
+        int num = rabbitsInForest.numRabbits(new int[] { 10, 10, 10 });
+        Assert.assertEquals(11, num);
+    }
+
+    @Test
+    public void test3() {
+        int num = rabbitsInForest.numRabbits(new int[] {});
+        Assert.assertEquals(0, num);
+    }
+
+    @Test
+    public void test4() {
+        int num = rabbitsInForest.numRabbits(new int[] { 1, 0, 1, 0, 0 });
+        Assert.assertEquals(5, num);
+    }
+
+    @Test
+    public void test5() {
+        int num = rabbitsInForest.numRabbits(new int[] { 0, 0, 1, 1, 1 });
+        Assert.assertEquals(6, num);
+    }
+
+}
