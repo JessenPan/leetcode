e6aa800b9c6024d7b5222f8a1fa3343d36fa799f
jessenpan <jessenpan@qq.com>
2019-08-05 23:42:35 +0800

    submit solution

--- algorithms/java/src/main/java/org/jessenpan/leetcode/greedy/S870AdvantageShuffle.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/greedy/S870AdvantageShuffle.java
@@ -1,6 +1,7 @@
 package org.jessenpan.leetcode.greedy;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * @author jessenpan
@@ -11,12 +12,43 @@ public class S870AdvantageShuffle {
     public int[] advantageCount(int[] A, int[] B) {
 
         Element[] elements = new Element[B.length];
-        int len=B.length;
+        int len = B.length;
         for (int i = 0; i < len; i++) {
-            
-            Element ele=new Element(B[i],i);
-            elements[i]=ele;
+            elements[i] = new Element(B[i], i);
         }
+        Arrays.sort(elements, Comparator.comparingInt(o -> o.val));
+        Arrays.sort(A);
+        int[] rst = new int[len];
+        int j = 0, i = 0;
+        for (; i < len; i++) {
+            if (j < len && elements[i].val < A[j]) {
+                rst[elements[i].pos] = A[j];
+                A[j] = -1;
+                j++;
+            } else {
+                while (j < len && elements[i].val >= A[j]) {
+                    j++;
+                }
+                if (j >= len) {
+                    break;
+                } else {
+                    rst[elements[i].pos] = A[j];
+                    A[j] = -1;
+                    j++;
+                }
+            }
+        }
+        j = 0;
+        while (i < len) {
+
+            while (A[j] == -1) {
+                j++;
+            }
+            rst[elements[i].pos] = A[j];
+            j++;
+            i++;
+        }
+        return rst;
     }
 
     static class Element {
--- algorithms/java/src/test/java/org/jessenpan/leetcode/greedy/S870AdvantageShuffleTest.java
+++ algorithms/java/src/test/java/org/jessenpan/leetcode/greedy/S870AdvantageShuffleTest.java
@@ -18,8 +18,14 @@ public class S870AdvantageShuffleTest {
 
     @Test
     public void test2() {
-        int[] ads = advantageShuffle.advantageCount(new int[] { 12,24,8,32 }, new int[] { 13,25,32,11 });
-        Assert.assertArrayEquals(new int[] { 24,32,8,12 }, ads);
+        int[] ads = advantageShuffle.advantageCount(new int[] { 12, 24, 8, 32 }, new int[] { 13, 25, 32, 11 });
+        Assert.assertArrayEquals(new int[] { 24, 32, 8, 12 }, ads);
+    }
+
+    @Test
+    public void test3() {
+        int[] ads = advantageShuffle.advantageCount(new int[] { 718967141, 189971378, 341560426, 23521218, 339517772 }, new int[] { 967482459, 978798455, 744530040, 3454610, 940238504 });
+        Assert.assertArrayEquals(new int[] { 718967141, 341560426, 339517772, 23521218, 189971378 }, ads);
     }
 
 }
