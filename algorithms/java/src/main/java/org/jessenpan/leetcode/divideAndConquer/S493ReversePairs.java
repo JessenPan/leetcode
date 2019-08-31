accf6e762ce48c0a12b416810a9e119876b02e62
jessenpan <jessenpan@qq.com>
2019-08-31 23:48:40 +0800

    submit solution

--- algorithms/java/src/main/java/org/jessenpan/leetcode/divideAndConquer/S493ReversePairs.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/divideAndConquer/S493ReversePairs.java
@@ -6,8 +6,52 @@ package org.jessenpan.leetcode.divideAndConquer;
  */
 public class S493ReversePairs {
 
+    private int pair = 0;
+
     public int reversePairs(int[] nums) {
-        return 1;
+        if(nums == null || nums != null && nums.length == 0)
+            return 0;
+        mergeSort(nums, 0, nums.length - 1);
+        return pair;
+    }
+
+    public void mergeSort(int[] nums,int start,int end) {
+        if(start == end)
+            return;
+        int mid = (start + end)/2;
+        mergeSort(nums, start, mid);
+        mergeSort(nums, mid + 1, end);
+        merge(nums, start, mid, mid + 1, end);
+    }
+
+    public void merge(int[] nums,int l1,int r1,int l2,int r2) {
+        int ptr1 = l1,ptr2 = l2;
+        while(ptr1 <= r1 && ptr2 <= r2) {
+            long leftVar = nums[ptr1],rightVar = 2*(long)nums[ptr2];
+            if(leftVar > rightVar) {
+                pair = pair + r1 - ptr1 + 1;
+                ptr2++;
+            }else {
+                ptr1++;
+            }
+        }
+        int[] newArr = new int[r1 - l1 + 1 + r2 - l2 + 1];
+        int idx1 = l1,idx2 = l2,i = 0;
+        while(idx1 <= r1 && idx2 <= r2) {
+            if(nums[idx1] >= nums[idx2]) {
+                newArr[i++] = nums[idx2++];
+            }else if(nums[idx1] < nums[idx2]) {
+                newArr[i++] = nums[idx1++];
+            }
+        }
+        while(idx1 <= r1)
+            newArr[i++] = nums[idx1++];
+        while(idx2 <= r2)
+            newArr[i++] = nums[idx2++];
+        int y = 0;
+        for(int x = l1; x <= r2; x++) {
+            nums[x] = newArr[y++];
+        }
     }
 
 }
