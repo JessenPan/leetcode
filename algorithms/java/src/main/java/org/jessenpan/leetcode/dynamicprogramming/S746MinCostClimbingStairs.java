afe8d66f8a687fcac2002514bb4ef39244418052
jessenpan <jessenpan@qq.com>
2019-09-02 00:05:29 +0800

    submit solution

--- algorithms/java/src/main/java/org/jessenpan/leetcode/dynamicprogramming/S746MinCostClimbingStairs.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/dynamicprogramming/S746MinCostClimbingStairs.java
@@ -6,4 +6,13 @@ package org.jessenpan.leetcode.dynamicprogramming;
  */
 public class S746MinCostClimbingStairs {
 
+    public int minCostClimbingStairs(int[] cost) {
+
+        int len = cost.length;
+        for (int i = 2; i < len; i++) {
+            cost[i] += Math.min(cost[i - 1], cost[i - 2]);
+        }
+        return Math.min(cost[len - 1], cost[len - 2]);
+    }
+
 }
