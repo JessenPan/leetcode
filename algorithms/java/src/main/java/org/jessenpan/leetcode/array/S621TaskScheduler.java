d8a18bd176f0df7ce9f3c91a7ee7b34b1071083c
jessenpan <jessenpan@qq.com>
2019-09-26 23:18:57 +0800

    modify package name

--- algorithms/java/src/main/java/org/jessenpan/leetcode/array/S621TaskScheduler.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/array/S621TaskScheduler.java
@@ -3,12 +3,11 @@ package org.jessenpan.leetcode.array;
 /**
  * @author jessenpan
  * tag:array
- * 
  **/
 public class S621TaskScheduler {
 
     public int leastInterval(char[] tasks, int n) {
-        
+        return 1;
     }
-    
+
 }
--- algorithms/java/src/main/java/org/jessenpan/leetcode/array/S974SubarraySumsDivisibleByK.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/array/S974SubarraySumsDivisibleByK.java
@@ -6,8 +6,18 @@ package org.jessenpan.leetcode.array;
  */
 public class S974SubarraySumsDivisibleByK {
 
-    public int subarraysDivByK(int[] nums, int k) {
-        return 1;
+    //TODO 
+    public int subarraysDivByK(int[] A, int K) {
+        int N = A.length, sum = 0, ans = 0;
+        int[] map = new int[K];
+        map[0] = 1;
+        for (int i = 1; i <= N; i++) {
+            sum = sum + A[i-1];
+            int key = (sum % K + K) % K;
+            ans += map[key];
+            map[key]++;
+        }
+        return ans;
     }
 
 }
