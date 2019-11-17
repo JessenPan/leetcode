6f12d5729545fad1f105cc9cd407aff9e77172e6
jessenpan <jessenpan@qq.com>
2019-11-17 23:14:04 +0800

    modify package name

---
+++ algorithms/java/src/test/java/org/jessenpan/leetcode/dynamicProgramming/S813LargestSumOfAveragesTest.java
@@ -0,0 +1,19 @@
+package org.jessenpan.leetcode.dynamicProgramming;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author jessenpan
+ */
+public class S813LargestSumOfAveragesTest {
+
+    private S813LargestSumOfAverages largestSumOfAverages = new S813LargestSumOfAverages();
+
+    @Test
+    public void test1() {
+        double max = largestSumOfAverages.largestSumOfAverages(new int[] { 9, 1, 2, 3, 9 }, 3);
+        Assert.assertEquals(20, max, 0);
+    }
+
+}
