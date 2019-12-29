11a9205265d83c8a731fc392d73d2dacc6a94f97
jessenpan <cdpanxinyu@jd.com>
2019-12-29 21:53:52 +0800

    init

--- algorithms/java/src/main/java/org/jessenpan/leetcode/queue/S622DesignCircularQueue.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/queue/S622DesignCircularQueue.java
@@ -6,7 +6,43 @@ package org.jessenpan.leetcode.queue;
  */
 public class S622DesignCircularQueue {
 
-    
+  public static   class MyCircularQueue {
+
+        /** Initialize your data structure here. Set the size of the queue to be k. */
+        public MyCircularQueue(int k) {
+
+        }
+
+        /** Insert an element into the circular queue. Return true if the operation is successful. */
+        public boolean enQueue(int value) {
+
+        }
+
+        /** Delete an element from the circular queue. Return true if the operation is successful. */
+        public boolean deQueue() {
+
+        }
+
+        /** Get the front item from the queue. */
+        public int Front() {
+
+        }
+
+        /** Get the last item from the queue. */
+        public int Rear() {
+
+        }
+
+        /** Checks whether the circular queue is empty or not. */
+        public boolean isEmpty() {
+
+        }
+
+        /** Checks whether the circular queue is full or not. */
+        public boolean isFull() {
+
+        }
+    }
 
 
 }
