a822e7686ab10ace6bd5d48b7c31d94320750eee
jessenpan <cdpanxinyu@jd.com>
2019-12-29 22:38:23 +0800

    init

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/random/S528RandomPickWithWeight.java
@@ -0,0 +1,20 @@
+package org.jessenpan.leetcode.random;
+
+/**
+ * @author jessenpan
+ * tag:random
+ */
+public class S528RandomPickWithWeight {
+
+    static class Solution {
+
+        public Solution(int[] w) {
+
+        }
+
+        public int pickIndex() {
+
+        }
+    }
+
+}
