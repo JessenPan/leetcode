30b628e4d5374a60df611a3826e8a30af91e37f6
jessenpan <jessenpan@qq.com>
2020-02-27 17:37:20 +0800

    modify package name

--- algorithms/java/src/main/java/org/jessenpan/leetcode/math/S754ReachANumber.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/math/S754ReachANumber.java
@@ -9,11 +9,12 @@ public class S754ReachANumber {
     //TODO study
     public int reachNumber(int target) {
         target = Math.abs(target);
-        int k = 0;
-        while (target > 0) {
-            target -= ++k;
+        int p = 0, step = 0;
+        while (p < target || (p + target) % 2 != 0) {
+            step += 1;
+            p = p + step;
         }
-        return target % 2 == 0 ? k : k + 1 + k % 2;
+        return step;
     }
 
 }
