d4bd744791b342a71a7c3ab57cc9f3cee2e62151
jessenpan <jessenpan@qq.com>
2020-03-02 15:55:33 +0800

    modify package name

--- algorithms/java/src/main/java/org/jessenpan/leetcode/minimax/S843GuessTheWord.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/minimax/S843GuessTheWord.java
@@ -14,29 +14,42 @@ public class S843GuessTheWord {
         int guess(String word);
     }
 
+    //TODO study
+
     int[][] H;
+
     public void findSecretWord(String[] wordlist, Master master) {
         int N = wordlist.length;
         H = new int[N][N];
-        for (int i = 0; i < N; ++i)
+        for (int i = 0; i < N; ++i) {
             for (int j = i; j < N; ++j) {
                 int match = 0;
-                for (int k = 0; k < 6; ++k)
-                    if (wordlist[i].charAt(k) == wordlist[j].charAt(k))
+                for (int k = 0; k < 6; ++k) {
+                    if (wordlist[i].charAt(k) == wordlist[j].charAt(k)) {
                         match++;
+                    }
+                }
                 H[i][j] = H[j][i] = match;
             }
+        }
 
-        List<Integer> possible = new ArrayList();
-        List<Integer> path = new ArrayList();
-        for (int i = 0; i < N; ++i) possible.add(i);
+        List<Integer> possible = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        for (int i = 0; i < N; ++i)
+            possible.add(i);
 
         while (!possible.isEmpty()) {
             int guess = solve(possible, path);
             int matches = master.guess(wordlist[guess]);
-            if (matches == wordlist[0].length()) return;
-            List<Integer> possible2 = new ArrayList();
-            for (Integer j: possible) if (H[guess][j] == matches) possible2.add(j);
+            if (matches == wordlist[0].length()) {
+                return;
+            }
+            List<Integer> possible2 = new ArrayList<>();
+            for (Integer j : possible) {
+                if (H[guess][j] == matches) {
+                    possible2.add(j);
+                }
+            }
             possible = possible2;
             path.add(guess);
         }
@@ -44,22 +57,30 @@ public class S843GuessTheWord {
     }
 
     public int solve(List<Integer> possible, List<Integer> path) {
-        if (possible.size() <= 2) return possible.get(0);
+        if (possible.size() <= 2) {
+            return possible.get(0);
+        }
         List<Integer> ansgrp = possible;
         int ansguess = -1;
 
         for (int guess = 0; guess < H.length; ++guess) {
             if (!path.contains(guess)) {
-                ArrayList<Integer>[] groups = new ArrayList[7];
-                for (int i = 0; i < 7; ++i) groups[i] = new ArrayList<Integer>();
-                for (Integer j: possible) if (j != guess) {
-                    groups[H[guess][j]].add(j);
+                List<Integer>[] groups = new ArrayList[7];
+                for (int i = 0; i < 7; ++i) {
+                    groups[i] = new ArrayList<>();
+                }
+                for (Integer j : possible) {
+                    if (j != guess) {
+                        groups[H[guess][j]].add(j);
+                    }
                 }
 
-                ArrayList<Integer> maxgroup = groups[0];
-                for (int i = 0; i < 7; ++i)
-                    if (groups[i].size() > maxgroup.size())
+                List<Integer> maxgroup = groups[0];
+                for (int i = 0; i < 7; ++i) {
+                    if (groups[i].size() > maxgroup.size()) {
                         maxgroup = groups[i];
+                    }
+                }
 
                 if (maxgroup.size() < ansgrp.size()) {
                     ansgrp = maxgroup;
