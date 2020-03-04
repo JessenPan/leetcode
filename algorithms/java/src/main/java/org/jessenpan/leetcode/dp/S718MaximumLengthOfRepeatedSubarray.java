2deba8bfc9ca0f755817a22f04150a24fd474044
jessenpan <jessenpan@qq.com>
2020-03-04 21:35:51 +0800

    modify package name

--- algorithms/java/src/main/java/org/jessenpan/leetcode/dp/S718MaximumLengthOfRepeatedSubarray.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/dp/S718MaximumLengthOfRepeatedSubarray.java
@@ -7,7 +7,18 @@ package org.jessenpan.leetcode.dp;
 public class S718MaximumLengthOfRepeatedSubarray {
 
     public int findLength(int[] a, int[] b) {
-        return 1;
+
+        int aLen = a.length, bLen = b.length, max = 0;
+        int[][] dp = new int[aLen + 1][bLen + 1];
+        for (int i = 0; i < aLen; i++) {
+            for (int j = 0; j < bLen; j++) {
+                if (a[i] == b[j]) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                    max = Math.max(dp[i + 1][j + 1], max);
+                }
+            }
+        }
+        return max;
     }
 
 }
