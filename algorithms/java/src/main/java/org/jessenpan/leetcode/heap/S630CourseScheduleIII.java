fce0af93e7741acbe8cfcdfef988f4f4826bfe42
jessenpan <jessenpan@qq.com>
2020-03-14 23:00:08 +0800

    modify package name

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/heap/S630CourseScheduleIII.java
@@ -0,0 +1,27 @@
+package org.jessenpan.leetcode.heap;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/**
+ * @author jessenpan
+ */
+public class S630CourseScheduleIII {
+
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
+        int time = 0;
+        for (int[] c : courses) {
+            if (time + c[0] <= c[1]) {
+                queue.offer(c[0]);
+                time += c[0];
+            } else if (!queue.isEmpty() && queue.peek() > c[0]) {
+                time += c[0] - queue.poll();
+                queue.offer(c[0]);
+            }
+        }
+        return queue.size();
+    }
+    
+}
