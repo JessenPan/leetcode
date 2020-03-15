2fea01d37da85ec7de7b9c46ecd1a45e9454c26f
jessenpan <jessenpan@qq.com>
2020-03-15 22:47:28 +0800

    modify package name

--- algorithms/java/src/main/java/org/jessenpan/leetcode/greedy/S948BagOfTokens.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/greedy/S948BagOfTokens.java
@@ -1,13 +1,33 @@
 package org.jessenpan.leetcode.greedy;
 
+import java.util.Arrays;
+
 /**
  * @author jessenpan
  * tag:greedy
  */
 public class S948BagOfTokens {
 
-    public int bagOfTokensScore(int[] tokens, int P) {
-        return 1;
+    public int bagOfTokensScore(int[] tokens, int energy) {
+
+        Arrays.sort(tokens);
+        int left = 0, right = tokens.length - 1;
+        int points = 0, score = 0;
+        while (left <= right && (energy >= tokens[left] || points > 0)) {
+            while (left <= right && energy >= tokens[left]) {
+                energy -= tokens[left++];
+                points++;
+            }
+
+            score = Math.max(score, points);
+            if (left <= right && points > 0) {
+                energy += tokens[right--];
+                points--;
+            }
+        }
+
+        return score;
+
     }
 
 }
