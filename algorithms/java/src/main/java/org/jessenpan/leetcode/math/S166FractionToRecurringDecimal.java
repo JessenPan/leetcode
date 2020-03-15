1e05f9c0b5158f8db74e241fe693afb142b27528
jessenpan <jessenpan@qq.com>
2020-03-15 21:21:26 +0800

    modify package name

---
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/math/S166FractionToRecurringDecimal.java
@@ -0,0 +1,46 @@
+package org.jessenpan.leetcode.math;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author jessenpan
+ * tag:math
+ */
+public class S166FractionToRecurringDecimal {
+
+    //TODO study
+    public String fractionToDecimal(int numerator, int denominator) {
+        if (numerator == 0) {
+            return "0";
+        }
+        StringBuilder fraction = new StringBuilder();
+        // If either one is negative (not both)
+        if (numerator < 0 ^ denominator < 0) {
+            fraction.append("-");
+        }
+        // Convert to Long or else abs(-2147483648) overflows
+        long dividend = Math.abs(Long.valueOf(numerator));
+        long divisor = Math.abs(Long.valueOf(denominator));
+        fraction.append(dividend / divisor);
+        long remainder = dividend % divisor;
+        if (remainder == 0) {
+            return fraction.toString();
+        }
+        fraction.append(".");
+        Map<Long, Integer> map = new HashMap<>();
+        while (remainder != 0) {
+            if (map.containsKey(remainder)) {
+                fraction.insert(map.get(remainder), "(");
+                fraction.append(")");
+                break;
+            }
+            map.put(remainder, fraction.length());
+            remainder *= 10;
+            fraction.append(remainder / divisor);
+            remainder %= divisor;
+        }
+        return fraction.toString();
+    }
+
+}
