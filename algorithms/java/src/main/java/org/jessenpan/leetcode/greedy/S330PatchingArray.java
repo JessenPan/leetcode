c09dc4077b12c2ec3d7c38b232ffffa630e00ad8
jessenpan <jessenpan@qq.com>
2020-03-15 13:43:06 +0800

    modify package name

--- algorithms/java/src/main/java/org/jessenpan/leetcode/greedy/S330PatchingArray.java
+++ algorithms/java/src/main/java/org/jessenpan/leetcode/greedy/S330PatchingArray.java
@@ -6,8 +6,19 @@ package org.jessenpan.leetcode.greedy;
  */
 public class S330PatchingArray {
 
+    //TODO study
     public int minPatches(int[] nums, int n) {
-        return 1;
+        int patches = 0, i = 0;
+        long miss = 1; // use long to avoid integer overflow error
+        while (miss <= n) {
+            if (i < nums.length && nums[i] <= miss) // miss is covered
+                miss += nums[i++];
+            else { // patch miss to the array
+                miss += miss;
+                patches++; // increase the answer
+            }
+        }
+        return patches;
     }
 
 }
